ac5f2829e7793f3d54bcf7aef75c67ba8c9870c7
shenshenwu <1973739324@qq.com>
2020-01-12 21:20:26 +0800

    load

---
+++ command.sh
@@ -0,0 +1,30 @@
+
+### 初始化git库
+git init
+
+### 查看git状态
+git status
+
+### 将文件添加到仓库
+git add file
+
+### 将文件提交到仓库
+git commit -m "some message"
+
+### 查看更改日志
+git log
+
+### 查看更改日志-简洁版
+git log --pretty=oneline
+
+### 版本恢复
+git reset --hard XXX
+
+### 查看文件差异
+git diff file
+
+### 丢弃工作区的修改
+git checkout --file
+
+### 删除文件
+git rm file
